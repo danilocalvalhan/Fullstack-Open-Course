60091759ff521aa75862a9cd1605c987c1d944e6
Danilo_Calvalhan <danilocalvalhan@hotmail.com>
2022-10-27 23:10:55 -0300

    2.4: Course information step9 and 2.5: separete module

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -2,12 +2,12 @@ import Header from './Header'
 import Content from './Content'
 import Total from './Total'
 
-const Course = ({course}) => {
+const Course = (props) => {
 	return (
 		<>
-			<Header text={course.name} />
-			<Content parts={course.parts} />
-			<Total parts={course.parts} />
+			<Header text={props.courseObj.name} />
+			<Content parts={props.courseObj.parts} />
+			<Total parts={props.courseObj.parts} />
 		</>	
 	)
 }
