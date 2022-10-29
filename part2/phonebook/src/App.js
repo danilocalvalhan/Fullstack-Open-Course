6ba98faf55edc17d5ef0787f641b30c5a97e7926
Danilo_Calvalhan <danilocalvalhan@hotmail.com>
2022-10-29 19:14:56 -0300

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,24 @@
 import { useState } from "react";
 
 const Numbers = ({ numbers }) => {
-	return numbers.map((number) => <p key={number.name}>{number.name}</p>);
+	return numbers.map((person) => (
+		<p key={person.name}>
+			{person.name} {person.number}
+		</p>
+	));
 };
 
 const App = () => {
-	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+	const [persons, setPersons] = useState([
+		{ name: "Arto Hellas", number: "040-1234567" },
+	]);
 	const [newName, setNewName] = useState("");
+	const [newNumber, setNewNumber] = useState("");
 
 	const addPerson = (event) => {
 		event.preventDefault();
-		if (newName === "") {
-			alert("Enter a new name");
+		if (newName === "" || newNumber === "") {
+			alert("Enter a new name and a new number");
 		} else {
 			const newPersonAlreadyInPhonebook = persons
 				.map((person) => person.name)
@@ -22,14 +29,19 @@ const App = () => {
 					`${newName} is already added to phonebook`
 				);
 			} else {
-				const personObj = { name: newName };
+				const personObj = {
+					name: newName,
+					number: newNumber,
+				};
 				setPersons(persons.concat(personObj));
 				setNewName("");
+				setNewNumber("");
 			}
 		}
 	};
 
 	const changeName = (event) => setNewName(event.target.value);
+	const changeNumber = (event) => setNewNumber(event.target.value);
 
 	return (
 		<div>
@@ -41,6 +53,14 @@ const App = () => {
 						type="text"
 						value={newName}
 						onChange={changeName}
+					/>
+				</div>
+				<div>
+					number:{" "}
+					<input
+						type="tel"
+						value={newNumber}
+						onChange={changeNumber}
 					/>
 				</div>
 				<div>
